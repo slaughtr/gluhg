f3549d7bd5eeddabfa44470828db141897e4dfd9
slaughtr <dslaughtr@gmail.com>
2017-06-27 21:35:09 -0700

    Fix import of material, minor code cleanup

--- app/keg.model.ts
+++ app/keg.model.ts
@@ -1,3 +1,11 @@
 export class Keg {
-  constructor(public name: string, public brewery: string, public price: number, public abv: number, public ibu: number, public style: string, public pintsConsumed: number, public pintsCapacity: number, public imgURL: string) {}
+  constructor(public name: string,
+              public brewery: string,
+              public price: number,
+              public abv: number,
+              public ibu: number,
+              public style: string,
+              public pintsConsumed: number,
+              public pintsCapacity: number,
+              public imgURL: string) {}
 }
